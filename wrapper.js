0e9cdcc76a567d22181f4977668ddfd20f600d76
edwardvalerio <edward.valerio@lc.cuny.edu>
2018-10-11 09:05:22 -0400

    Wrapper initial commit

---
+++ wrapper.js
@@ -0,0 +1,29 @@
+
+    var arr = [function() {
+        alert('before function call');
+    }, function() {
+        alert('after function call');
+    }];
+
+
+    function AttachMe(object, method, wrapper, arr) {
+        var fn = object[method];
+
+        return object[method] = function() {
+
+            return wrapper.apply(this, [fn.bind(this)].concat(
+                Array.prototype.slice.call(arguments), arr));
+        };
+    };
+
+    AttachMe(window, "test", function(orginalFn) {
+        var originalParams = Array.prototype.slice.call(arguments, 1);
+
+        console.log(originalParams);
+
+        var before = originalParams[0];
+        var after = originalParams[1];
+        before();
+        orginalFn.apply(undefined, originalParams);
+        after();
+    }, arr);
